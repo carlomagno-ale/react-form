10ee4f44ee11c94b4b14239dd0eaaaf4c27803d1
carlomagno-ale <carlomagno.ale@gmail.com>
2025-03-26 17:17:41 +0100

    implement article submission form and update article list management

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 
-const articlesList = ['Metaverso: Realtà?', 'Chip: La Rivoluzione', 'Auto: Guida Autonoma', 'Spazio: Nuove Frontiere', 'Robot: Vita Quotidiana', 'Dati: Il Nuovo Oro']
+const articleList = ['Metaverso: Realtà?', 'Chip: La Rivoluzione', 'Auto: Guida Autonoma', 'Spazio: Nuove Frontiere', 'Robot: Vita Quotidiana', 'Dati: Il Nuovo Oro']
 
 export default function App() {
 
+  const [newArticle, setNewArticle] = useState('')
+  const [articles, setArticles] = useState(articleList)
 
-  const [article, setArticles] = useState(articlesList)
+  function handleFormSubmit() {
+
+    setArticles([newArticle, ...articles])
+    setNewArticle('')
+
+  }
 
   return (
     <>
@@ -13,12 +20,31 @@ export default function App() {
 
         <h1 className='text-center my-5'>Articoli</h1>
 
+        <form onSubmit={handleFormSubmit}>
+          <input
+            type="text"
+            className="form-control"
+            name="new_article"
+            id="new_article"
+            placeholder="Scrivi il titolo del tuo articolo"
+            value={newArticle}
+            onChange={e => setNewArticle(e.target.value)}
+          />
+          <div className='d-flex justify-content-start my-2'>
+            <button type="submit" className="btn btn-success">
+              Submit
+            </button>
+          </div>
+        </form>
+
         <ul className="list-group">
-          {article.map((title, index) => (
-            <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+          {articles.map((title) => (
+            <li key={title}
+              className="list-group-item d-flex justify-content-between align-items-center">
               {title}
             </li>
           ))}
+
         </ul>
       </div>
     </>
