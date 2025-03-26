2269bf39d84f97feb79232e20a604c289ee55c6d
carlomagno-ale <carlomagno.ale@gmail.com>
2025-03-26 16:43:31 +0100

    add Bootstrap for styling and implement article list in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import { useState } from 'react'
 
+const articlesList = ['Metaverso: Realtà?', 'Chip: La Rivoluzione', 'Auto: Guida Autonoma', 'Spazio: Nuove Frontiere', 'Robot: Vita Quotidiana', 'Dati: Il Nuovo Oro']
 
-function App() {
+export default function App() {
+
+
+  const [article, setArticles] = useState(articlesList)
 
   return (
     <>
+      <div className="container">
 
-      <h1>Form</h1>
+        <h1 className='text-center my-5'>Articoli</h1>
+
+        <ul className="list-group">
+          {article.map((title, index) => (
+            <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+              {title}
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   )
 }
 
-export default App
+
+
